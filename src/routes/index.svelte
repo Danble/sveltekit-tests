b54f28e9e31b6532aad7ac5517146f1a6635db0b
Diego Córdova Nieto <diego@livingtongues.org>
2022-07-25 12:16:55 -0500

    tailwind installed

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 		'- **Preview:**  A live display of the generated HTML as it would render in a browser. <strong>I am strong</strong>';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <hr />
 <div>{@html marked(text)}</div>
